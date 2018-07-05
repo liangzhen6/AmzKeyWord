ef7d2ea1639822f764ba3cc8d90359c17cd45df0
shenzhenshihua <liangz@szmn.com.cn>
2018-07-05 18:00:52 +0800

    add

--- keywords.py
+++ keywords.py
@@ -27,7 +27,7 @@ def get_sheet_mes():
 
 def getdata(keyword):
 	wordurl = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias=aps&field-keywords=%s&rh=i:aps,k:%s'%(keyword, keyword)
-	html = request.get(wordurl,timeout = 5)
+	html = request.get(wordurl,timeout = 10)
 	html_soup = BeautifulSoup(html.text, 'lxml')
 	span = html_soup.find('span', id = 's-result-count')
 	if span != None:
@@ -41,38 +41,48 @@ def getdata(keyword):
 		print(values)
 		return values
 	else:#无搜索量或者是网络问题
-		print('无搜索量')
+		print('无搜索量',wordurl)
 		return ''
-
 
 x, y, z = get_sheet_mes()
 
 curr_num = 1      #当为第0行为表头
 word_num = len(y) #表格总长度
+
 while curr_num < word_num:
 	workbookCopy, wordlist, selllist = get_sheet_mes()
 	sheet = workbookCopy.get_sheet(0)
 	sells = selllist[curr_num]
+
 	if sells == '':
 		current = curr_num
 		for x in range(0,20):
 			current = curr_num+x
 			if current < word_num:
-				word = wordlist[current]
-				#可以抓取数据
-				print(current,word)
-				string = getdata(word)
-				if len(string) == 0:
-					sheet.write(current, 2, string)
-				else:
-					sheet.write(current, 2, int(string))
-				time.sleep(2)
+				currsells = selllist[current]
+				if currsells == '':
+					word = wordlist[current]
+					#可以抓取数据
+					print(current,word)
+					string = getdata(word)
+					if string == '':
+						sheet.write(current, 2, string)
+					else:
+						sheet.write(current, 2, int(string))
+					time.sleep(1)
 			else:
 				break
-		curr_num = current + 1
 		savexls()
+		curr_num = current + 1
 	else:
+		#直接定位到下一个为 空的位置
 		curr_num += 1
+		for x in range(curr_num,len(selllist)):
+			print(x)
+			curr_num = x
+			sell = selllist[x]
+			if sell == '':
+				break
 
 
 
